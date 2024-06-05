f0374d132fa5bf68fca9d3985db4c425726aa025
satoshi hata <stosto826@gmail.com>
2024-06-05 22:36:39 +0900

    wip

--- doc/src/examples/demo/main.ts
+++ doc/src/examples/demo/main.ts
@@ -5,7 +5,7 @@ import {shade} from './shading'
 import {postEffect} from './postEffect'
 import {getBlurPass} from './blur'
 
-export const CUBE_NUM = 10000
+export const CUBE_NUM = 5000
 export const LIGHT_NUM = 50
 export const LIGHT_RANGE = 3000
 export const MAX_HEIGHT = 200
